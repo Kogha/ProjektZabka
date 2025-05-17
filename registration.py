9facff84de6bf8340b5364e4fa1e1d3c8d1c4c79
Szymon-Bielicki <sb84239@student.uwb.edu.pl>
2025-05-17 19:48:02 +0200

    Update registration.py
    Pierwsza pełna wersja modułu "registration.py". Dodano integrację z modułem "load.py" oraz stosowne opisy funkcji w stylu Google.

--- registration.py
+++ registration.py
@@ -2,16 +2,43 @@ from random import randint
 from dostepneID import *
 from pandas import *
 from tkinter import *
+from load import *
 
-def dodaj_uzytkownika(imie, nazwisko, email, haslo, ID, okno):
-    file = read_csv("customers.csv")
+def dodaj_uzytkownika(customers, imie, nazwisko, email, haslo, ID, okno):
+    """
+    Dodaje nowego użytkownika do bazy danych.
+
+    Args:
+        customers (str): Ścieżka do pliku z danymi klientów.
+        imie (str): Imię użytkownika.
+        nazwisko (str): Nazwisko użytkownika.
+        email (str): Adres e-mail użytkownika.
+        haslo (str): Hasło użytkownika.
+        ID (int): Unikalne ID użytkownika.
+        okno (Tk): Instancja głównego okna aplikacji.
+
+    Returns:
+        None
+    """
+    file = read_csv(customers)
     nowy_uzytkownik = DataFrame([{"Imię": imie, "Nazwisko": nazwisko, "E-mail": email, "Hasło": haslo, "ID": ID}])
     file = concat([file, nowy_uzytkownik], ignore_index=True)
-    file.to_csv("customers.csv", index=False)
+    file.to_csv(customers, index=False)
     
     okno.destroy()
 
 def rejestracja():
+    """
+    Tworzy i uruchamia okno rejestracji użytkownika.
+
+    Funkcja inicjalizuje interfejs graficzny dla procesu rejestracji, 
+    pobiera dane użytkownika i przekazuje je do funkcji dodającej użytkownika do bazy danych.
+
+    Returns:
+        None
+    """
+    scieszki = list(get_database_path())
+    customers = scieszki[1]
     okno = Tk(className = "rejestracja")
     Label(okno, text='Imie').grid(row=0)
     Label(okno, text='Nazwisko').grid(row=1)
@@ -35,6 +62,6 @@ def rejestracja():
 
     generujID = randint(DID)
     #dodaj_uzytkownika(imie, nazwisko, email, haslo, generujID)
-    Button(okno, text="Zarejestruj", command=lambda: dodaj_uzytkownika(imie.get(), nazwisko.get(), email.get(), haslo.get(), generujID,okno)).grid(row=4, column=1)
+    Button(okno, text="Zarejestruj", command=lambda: dodaj_uzytkownika(customers, imie.get(), nazwisko.get(), email.get(), haslo.get(), generujID,okno)).grid(row=4, column=1)
 
     okno.mainloop()
