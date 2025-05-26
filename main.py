a7e60a78053c3f025215adf43455bc7960f2ddd6
Szymon-Bielicki <sb84239@student.uwb.edu.pl>
2025-05-26 18:32:27 +0200

    Dokumentacja main.py
    Dokumentacja funkcji main programu.

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import load
 from Gui import start_Gui
 
 if __name__ == "__main__":
+    """
+    Funkcja main programu.
+    
+    Ładuje dane i uruchamia okno menu głównego programu.
+    """
     load.load_data()
     start_Gui()
