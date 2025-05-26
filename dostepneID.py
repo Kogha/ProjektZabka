6668dd2dec56f5600a7d9cb0897490114b33b713
Szymon-Bielicki <sb84239@student.uwb.edu.pl>
2025-05-26 18:40:03 +0200

    Dokumentacja dostepneID.py
    Dodanie dokumentacji funkcji modułu "dostepneID.py".

--- dostepneID.py
+++ dostepneID.py
@@ -2,6 +2,15 @@ from pandas import *
 import load
 
 def dostepne_ID(DID):
+  """
+  Funkcja aktualizuje set wszystkich dostępnych ID.
+  
+  Args:
+    DID (set(int)): set wszystkich dostępnych ID.
+
+  Returns:
+    newDID (set(int)): zaktualizowany set wszystkich dostępnych ID.
+  """
   lines = load.get_database_path()
   filedata = read_csv(lines[1], encoding='cp1250')
   newDID = DID - set(filedata['ID'].values)
