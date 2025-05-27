56b5b86ec9ce9949c54167eb1be2656cfd222c4a
Szymon-Bielicki <sb84239@student.uwb.edu.pl>
2025-05-27 19:14:43 +0200

    Dodanie __init__.py
    Dodanie __init__.py

---
+++ __init__.py
@@ -0,0 +1,30 @@
+'''
+Help on package projektZabka:
+
+NAME
+projektZabka
+
+DESCRIPTION
+Aplikacja do zakupu produktów w sklepie Żabka.
+
+PACKAGE CONTAINS
+Gui (module)
+Zakup_produktow (module)
+database_module (module)
+dostepneID (module)
+load (module)
+logowanie (module)
+registration (module)
+shop_offer (module)
+main (module)
+Zabka1.png
+products.xlm
+customers.csv
+
+
+FILE
+https://github.com/Kogha/ProjektZabka
+
+'''
+
+__all__ = ["Gui","Zakup_produktow","database_module","dostepneID","load","logowanie","registration","shop_offer","main"]
